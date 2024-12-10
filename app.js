23b47ff0d0f073f4ef5b400696358fe0576fcea8
JacopoTrincanato <trincanatoj@gmail.com>
2024-12-10 15:28:15 +0100

    update show with reviews query

--- app.js
+++ app.js
@@ -47,16 +47,28 @@ app.get('/:id', (req, res) => {
     //creo una costante dove salvare l'id
     const id = req.params.id;
 
-    //connetto la query
+    //connetto la query dei film
     connection.query(sql, [id], (err, results) => {
         if (err) return res.status(500).json({
             error: err
         });
 
-        if (results.length === 0) return res.status(404).json({ error: 'Movie not found!' })
+        if (results.length === 0) return res.status(404).json({ error: 'Movie not found!' });
 
+        //creo una query per trovare le recensioni
+        const reviewSql = 'SELECT * FROM reviews WHERE movie_id = ?';
+
+        //connetto la query delle review
+        connection.query(reviewSql, [id], (err, reviewsResults) => {
+            if (err) return res.status(500).json({ error: err });
+
+            //associo i film alle review
+            const movie = {
+                ...results[0],
+                reviews: reviewsResults
+            };
+
+            res.json(movie);
+        });
     });
-
-    //creo una query per trovare le recensioni
-    const reviewSql = 'SELECT * FROM reviews WHERE movie_id = ?';
 });
