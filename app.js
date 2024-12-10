85ec3c4312e429957a522a8ee91b4e8a5db57eb7
JacopoTrincanato <trincanatoj@gmail.com>
2024-12-10 15:13:42 +0100

    create index in app.js

--- app.js
+++ app.js
@@ -10,7 +10,30 @@ const HOST = process.env.HOST;
 //creo una variabile per la PORT
 const PORT = process.env.PORT;
 
+//importo la connessione
+const movieConnection = require('./db/moviesConnection.js');
+const connection = require('./db/moviesConnection.js');
+
 //faccio girare il server
 app.listen(PORT, () => {
     console.log(`Server is running at ${HOST}:${PORT}`);
+});
+
+//creo una rotta index
+app.get('/', (req, res) => {
+
+    //creo una costante per la query
+    const sql = 'SELECT * FROM movies';
+
+    //connetto la query
+    connection.query(sql, (err, results) => {
+        if (err) return res.status(500).json({
+            error: err
+        });
+
+        res.json({
+            movies: results,
+            count: results.length
+        })
+    });
 });
